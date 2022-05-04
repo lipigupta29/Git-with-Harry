ad62332b8b8cfc4d6677ee3179350651a79d63c7
Lipi <sanlmlipi@gmail.com>
2022-05-04 10:21:08 +0530

    Added comment

--- NEW.py
+++ NEW.py
@@ -2,3 +2,10 @@ print("""Five little monkeys jumping on the bed,
 One fell down and bumped his head,
 Mama called the doctor and the doctor said,
 No more monkeys jumping on the bed!""")
+
+print("""The doors on the bus go open and shut;
+Open and shut;
+Open and shut.
+The doors on the bus go open and shut;
+all through the town.
+Push hands back and forth in front of you""")
