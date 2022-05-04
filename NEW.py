7b76880ee0196ee48191836f80c8fd9d275c9db3
Lipi <sanlmlipi@gmail.com>
2022-05-04 10:18:01 +0530

    fixed extra things

--- NEW.py
+++ NEW.py
@@ -1,7 +1,4 @@
 print("""Five little monkeys jumping on the bed,
 One fell down and bumped his head,
 Mama called the doctor and the doctor said,
-No more monkeys jumping on the bed!
-""")
-
-print("hipo bipo")
+No more monkeys jumping on the bed!""")
