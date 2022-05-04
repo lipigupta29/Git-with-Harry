0903282a565ea046eb80fdf276319d83b681e4fd
Lipi <sanlmlipi@gmail.com>
2022-05-04 09:51:36 +0530

    waste1

--- contact.py
+++ contact.py
@@ -3,10 +3,3 @@ Humpty Dumpty had a great fall.
 All the king's horses and all the king's men
 Couldn't put Humpty together again.""")
 
-print("""Row, row, row your boat
-Gently up the creek If you see a little mouse
-Don’t forget to squeak!
-
-Row, row, row your boat
-Gently down the stream If you see a crocodile
-Don’t forget to scream!""")
