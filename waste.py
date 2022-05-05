e80f90ef6e7d074a6f755d40a6ea3d82b3451daa
Lipi <sanlmlipi@gmail.com>
2022-05-05 08:34:37 +0530

    added a file waste.py

---
+++ waste.py
@@ -0,0 +1,3 @@
+print("""There was a farmer who had a dog, And Bingo was his name-O. B-I-N-G-O! B-I-N-G-O! B-I-N-G-O! And Bingo was his name-O!
+
+There was a farmer who had a dog, And Bingo was his name-O. [Clap]-I-N-G-O! [Clap]-I-N-G-O! [Clap]-I-N-G-O! And Bingo was his name-O!""")
